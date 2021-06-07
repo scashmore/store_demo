d5462a6da57a4d6dd71fe48c851f4482eef1321e
Susan Ashmore <scashmorecitcom@gmail.com>
2021-06-07 19:50:56 -0400

    sticky navbar

--- store/src/components/Navbar/Navbar.jsx
+++ store/src/components/Navbar/Navbar.jsx
@@ -12,7 +12,7 @@ const Navbar = ({totalItems}) => {
 
     return (
         <>
-          <AppBar position='fixed' className={classes.appBar} color="inherit">
+          <AppBar position='sticky' className={classes.appBar} color="inherit">
             <Toolbar>
                 <Typography component={Link} to="/" variant='h6' className={classes.title} color="inherit">
                     <img src={logo} alt="store.js" height="25px" className={classes.image}/>
--- store/src/components/Products/Product/styles.js
+++ store/src/components/Products/Product/styles.js
@@ -1,20 +1,21 @@
-import {makeStyles} from '@material-ui/core/styles';
+
+import { makeStyles } from '@material-ui/core/styles';
 
 export default makeStyles(() => ({
-    root: {
-        maxWidth: '100%',
-        margin: '3px',
-    },
-    media: {
-        height: 0,
-        paddingTop: '56.25%',
-    },
-    cardActions: {
-        display: 'flex',
-        justifyContent: 'flex-end',
-    },
-    cardContent: {
-        display: 'flex',
-        justifyContent: 'space-between',
-    },
-}))
+  root: {
+    margin: '5px',
+    maxWidth: '100%',
+  },
+  media: {
+    height: 0,
+    paddingTop: '56.25%',
+  },
+  cardActions: {
+    display: 'flex',
+    justifyContent: 'flex-end',
+  },
+  cardContent: {
+    display: 'flex',
+    justifyContent: 'space-between',
+  },
+}));
