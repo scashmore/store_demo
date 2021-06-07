71926b333e8f67f9eb9f4ad1bacd2c69e21f590b
Susan Ashmore <scashmorecitcom@gmail.com>
2021-06-07 14:56:39 -0400

    product styles

--- store/src/components/Products/Product/Product.jsx
+++ store/src/components/Products/Product/Product.jsx
@@ -1,13 +1,31 @@
 import React from 'react'
-import {Card, CardMedi, CardContent, CardActinos, Typography, IconButton} from '@material-ui-core'
+import {Card, CardMedia, CardContent, CardActinos, Typography, IconButton} from '@material-ui-core'
 import {AddShoppingCart} from '@material-ui/icons'
+import useStyles from './styles'
 
-
-const Product = () => {
+const Product = ({product}) => {
+    const classes = useStyles();
     return (
         <Card className={classes.root}>
-            <CardMedia className={classes.media} image='' title={Product.name} />
-
+            <CardMedia className={classes.media} image='' title={product.name} />
+            <CardContent>
+                <div className={classes.cardContent}>
+                    <Typography variant='h5' gutterBottom>
+                        {product.name}
+                    </Typography>
+                    <Typography variant='h5'>
+                        {product.price}
+                    </Typography>
+                </div>
+                <Typography carriant='h2' color='textSecondary'>
+                    {product.description}
+                </Typography>
+            </CardContent>
+            <CardActinos disableSpacing className={classes.cardActions}>
+                <IconButton area-label="Add to Cart">
+                    <AddShoppingCart />
+                </IconButton>
+            </CardActinos>
         </Card>
     )
 }
