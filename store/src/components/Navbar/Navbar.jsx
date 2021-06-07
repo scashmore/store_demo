ffe88ffb3d52d11d144222a71ccd62df4a5b6007
Susan Ashmore <scashmorecitcom@gmail.com>
2021-06-07 15:58:54 -0400

    badge content edit

--- store/src/components/Navbar/Navbar.jsx
+++ store/src/components/Navbar/Navbar.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import { AppBar, Toolbar, IconButton, Badge, MenuItem, Menu, Typography} from '@material-ui/core';
-import { ShoppingCart } from '@material-ui.icons'
+import { ShoppingCart } from '@material-ui/icons'
 
 import logo from '../../assets/coffee.png'
+import useStyles from './styles'
 const Navbar = () => {
+    const classes = useStyles();
     return (
         <>
           <AppBar position='fixed' className={classes.appBar} color="inherit">
@@ -15,7 +17,7 @@ const Navbar = () => {
                 <div className={classes.grow} />
                 <div className={classes.button}>
                     <IconButton aria-label="Show cart items" color="inherit">
-                        <Badge badageContent={2} color='secondary'>
+                        <Badge badgeContent={2} color='secondary'>
                             <ShoppingCart />
                         </Badge>
                     </IconButton>
