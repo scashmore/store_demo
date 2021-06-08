f0da501c446ded43b7ea04b1bd3befd1c68c9277
Susan Ashmore <scashmorecitcom@gmail.com>
2021-06-08 12:55:20 -0400

    stripe

--- store/src/components/CheckoutForm/Checkout/Checkout.jsx
+++ store/src/components/CheckoutForm/Checkout/Checkout.jsx
@@ -40,7 +40,7 @@ const Checkout = ({cart}) => {
 
     const Form = () => activeStep === 0
         ? <AddressForm checkoutToken={checkoutToken} next={next}/>
-        : <PaymentForm  shippingData={shippingData} />
+        : <PaymentForm  shippingData={shippingData} checkoutToken={checkoutToken} backStep={backStep}/>
 
     return (
         <div>
--- store/src/components/CheckoutForm/Checkout/PaymentForm.jsx
+++ store/src/components/CheckoutForm/Checkout/PaymentForm.jsx
@@ -1,10 +1,39 @@
-import React from 'react'
+import React from 'react';
+import {Typography, Button, Divider } from '@material-ui/core';
+import {Elements, CardElement, ElementsConsumer} from '@stripe/react-stripe-js';
+import {loadStripe} from '@stripe/stripe-js';
 
-const PaymentForm = () => {
+import Review from './Review'
+
+const stripePromise = loadStripe('process.env.REACT_APP_STRIPE_KEY')
+
+const PaymentForm = ({checkoutToken, backStep}) => {
+    const handleSubmit = () => {
+        
+    }
     return (
-        <div>
-            
-        </div>
+        <>
+            <Review checkoutToken={checkoutToken}/>
+            <Divider />
+            <Typography variant="h6" gutterBottom style={{margin: '20px 0'}}>Payment Methood</Typography>
+            <Elements stripe={stripePromise}>
+                <ElementsConsumer>
+                    {({ elements, stripe}) => (
+                        <form>
+                            <CardElement />
+                            <br />
+                            <br />
+                            <div style={{display: 'flex', justifyContent: 'space-between'}}>
+                                <Button variant="outlined" onClick={backStep}>Back</Button>
+                                <Button tupe="submit" variant="contained" disabled={!stripe} color='primary'>
+                                    Pay {checkoutToken.live.subtotal.formatted_with_symbol}
+                                </Button>
+                            </div>
+                        </form>
+                    )}
+                </ElementsConsumer>
+            </Elements>
+        </>
     )
 }
 
